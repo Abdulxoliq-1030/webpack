9ea01243e4433182cceb332370d1cc5d0a34a8c2
Abdulxoliq Odiljonov <abdulxoliqodiljonov@Abdulxoliqs-MacBook-Pro.local>
2023-10-13 19:50:46 +0500

    webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,42 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // mode
   mode: "development", // production
   // entry
-  entry: path.resolve(__dirname, "./src/app.js"),
+  entry: {
+    app: path.resolve(__dirname, "./src/app.js"),
+    about: path.resolve(__dirname, "./src/about.js"),
+  },
   // output
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    filename: "[name].js",
   },
   // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // title,
+      title: "Webpack 5",
+      // filename,
+      filename: "index.html",
+      // template
+      template: "./src/temp.html",
+      // chunks
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      // title,
+      title: "About",
+      // filename,
+      filename: "about.html",
+      // template
+      template: "./src/tempAbout.html",
+      // chunks
+      chunks: ["about"],
+    }),
+  ],
   // loaders
   module: {
     rules: [
